facc5e94518f4336a2c241e2d18ba990bf22c6e0
Asocia <akkayas17@itu.edu.tr>
2021-01-10 19:59:35 +0300

    Görevler güncellendi

--- a04_alan_hesaplama.py
+++ a04_alan_hesaplama.py
@@ -11,15 +11,28 @@ Alan 6.25
 from turkish import *
 
 #################################
-uzunluk=noktalısayı(değeral("Bahçenin bir kenar uzunluğunu giriniz: "))
-alan=uzunluk*uzunluk
-
-
-
-
-
-
+# uzunluk=noktalısayı(değeral("Bahçenin bir kenar uzunluğunu giriniz: "))
+# alan=uzunluk*uzunluk
 
 
 #################################
+"""Görevin ilk aşamasını başarıyla tamamladın. İkinci
+aşamada bir dairenin alanını hesaplaman gerekiyor. 
+Yarıçapının uzunluğu r olan bir dairenin alanı π*r²'dir.
+π -> pi diye okunur. Değeri 3.141... diye sonsuza kadar
+gider. Bilgisayar sonsuz uzunluğundaki bir sayıyı belleğinde
+tutamadığı için yaklaşık bir değer kullanman gerekecek. 
+Bu değere "pi" ismiyle ulaşabilirsin.
+
+Dairenin yarıçapını giriniz: 3
+Alan: 28.27
+"""
+
+from math import pi
+##################################
+
+
+
+##################################
+
 yazdır(f"Alan: {alan:.2f}")
---
+++ a05_manav.py
@@ -0,0 +1,33 @@
+"""
+Esengül'ün alışveriş yaptığı manav artık yaşlandığı için hesap yaparken zorlanıyor.
+Sadece meyvelerin üstündeki fiyat etiketlerinden meyvenin kilosunun kaça olduğunu
+müşterilerine söyleyip kaç kilo almak istediklerini soruyor. Sonra toplam fiyatı
+müşteriye söylüyor. Müşteriden aldığı paradan toplam fiyatı çıkarıp para üstü veriyor.
+Görevin bu işlemleri yapması için manava yardım edecek bir program yazmak. 
+
+Elmanın kilosu ... lira, muzun kilosu ... lira.
+Kaç kilo elma alacaksınız? ...
+Kaç kilo muz alacaksınız? ...
+Toplam ... TL tuttu.
+Kaç para veriyorsunuz? ...
+Para üstünüz ... TL'dir.
+Yine bekleriz.
+"""
+
+from turkish import *
+
+elma_kg = 2.5
+muz_kg = 8
+
+##########################################
+
+
+
+
+
+
+##########################################
+
+yazdır(f"Para üstünüz {para_üstü:.2f} TL'dir")
+yazdır('Yine bekleriz.')
+
