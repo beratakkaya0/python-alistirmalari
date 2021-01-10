5d5b5b7572538ea4b4006f7dc5121640aadefcda
Asocia <akkayas17@itu.edu.tr>
2021-01-10 11:14:37 +0300

    Görevler güncellendi

--- a02_selamlaşma.py
+++ a02_selamlaşma.py
@@ -5,5 +5,21 @@ Merhaba Şahin.
 """
 
 from turkish import *
-isim=değeral("Merhaba, ben Python. Senin adın ne?\n")
-yazdır(f"Merhaba {isim}.")
+# isim=değeral("Merhaba, ben Python. Senin adın ne?\n")
+# yazdır(f"Merhaba {isim}.")
+
+######################################################
+"""Görevin ilk aşamasını başarıyla tamamladın. Şimdi
+aynı görevi farklı bir yöntemle yapman gerekiyor. 
+Sadece ### ile işaretlenmiş bölgeler arasına kod 
+yazabilirsin.
+"""
+
+
+
+
+
+
+
+#######################################################
+yazdır(selamlama_yazısı)
---
+++ a03_yaş_hesaplama.py
@@ -0,0 +1,11 @@
+"""Görevin kullanıcının yaşını hesaplamak.
+Adınızı giriniz: Fahri
+Doğum yılınızı giriniz: 2004
+17 yaşındasın Fahri.
+"""
+
+from turkish import *
+from datetime import datetime
+
+şimdiki_yıl = datetime.now().year
+#################################
