05bbc8f1f6d5be6ef66b3b6d3e08cfb4f0f91303
Asocia <akkayas17@itu.edu.tr>
2021-01-12 00:01:26 +0300

    Görevler güncellendi

--- a06_para_babası.py
+++ a06_para_babası.py
@@ -9,17 +9,23 @@ Her bir çocuğa 20 TL düşüyor.
 from turkish import *
 
 ################################################
-para=noktalısayı(değeral("Kaç paran var Abidin?\n "))
-kişi_başı_para=para/4/3
-
-
-
-
-
-
-
-
+# para=noktalısayı(değeral("Kaç paran var Abidin?\n "))
+# kişi_başı_para=para/4/3
 
 #################################################
 
-yazdır(f"Her bir çocuğa {kişi_başı_para:.3g} TL düşüyor")
+# yazdır(f"Her bir çocuğa {kişi_başı_para:.3g} TL düşüyor")
+
+"""Bu kolaydı! Hadi şimdi biraz matematik yapalım. Abidin *aynı* parayı eşit paylaştırmak
+yerine çocuklarının yaşlarıyla doğru orantılı olacak şekilde paylaştırmak istiyor.
+Görevin Abidin'e çocuklarının yaşını sorup parayı yaşlarla doğru orantılı olacak şekilde 
+paylaştırmak. Unutma, Abidin cebindeki paranın çeyreğini paylaştırmak istiyor!
+
+Helin kaç yaşında? 10
+Selin kaç yaşında? 14
+Pelin kaç yaşında? 8
+Kaç paran var Abidin? 256
+Helin 20 TL alacak.
+Selin 28 TL alacak.
+Pelin 16 TL alacak.
+"""
