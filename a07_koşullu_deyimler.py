a099112a0a92fba5bff24eae272b9418b3b2aaa4
Asocia <akkayas17@itu.edu.tr>
2021-01-14 19:08:44 +0300

    Görevler güncellendi

--- a07_koşullu_deyimler.py
+++ a07_koşullu_deyimler.py
@@ -13,9 +13,26 @@ from turkish import *
 
 #############################################
 
-yaş=değeral("Kaç yaşındasınız? ")
-if yaş>=18:
-    yazdır("Oy kullanabilirsiniz.")
-else:
-    yazdır("Oy kullanamazsınız.")
+# yaş=değeral("Kaç yaşındasınız? ")
+# if yaş>=18:
+    # yazdır("Oy kullanabilirsiniz.")
+# else:
+    # yazdır("Oy kullanamazsınız.")
+
+#############################################
+"""
+
+Görevin kullanıdan kullanıcı adı ve parola alıp toplam uzunluğu 40'tan az
+ise sisteme kabul eden bir program yazmak.
+
+Kullanıcı adınız: Şahin
+Parolanız       : 123456
+Kullanıcı adı ve parolanız toplam 11 karakterden oluşuyor!
+Sisteme hoşgeldiniz!
+
+Kullanıcı adınız: Çok uzun bir kullanıcı adı
+Parolanız       : Çok uzun bir parola
+Kullanıcı adı ve parolanız toplam 45 karakterden oluşuyor!
+Kullanıcı adınız ile parolanızın  toplam uzunluğu 40 karakteri geçmemeli!
+"""
 
