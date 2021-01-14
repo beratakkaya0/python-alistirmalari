92989bea856da61857f6c2b63df4d8be6445de21
Asocia <akkayas17@itu.edu.tr>
2021-01-14 18:29:49 +0300

    Görevler güncellendi

---
+++ a07_koşullu_deyimler.py
@@ -0,0 +1,15 @@
+"""
+Görevin kullancının yaşını sorup oy kullanabilme durumunu yazdıran bir 
+program yazmak.
+
+Kaç yaşındasınız? 14
+Oy kullanamazsınız.
+
+Kaç yaşındasınız? 20
+Oy kullanabilirsiniz.
+"""
+
+from turkish import *
+
+#############################################
+
