e64ec0b9ec693b7ef6a68e8c308477f75b69665b
Asocia <akkayas17@itu.edu.tr>
2021-01-18 12:43:45 +0300

    Görevler güncellendi

---
+++ a08_gelişmiş_işleçler.py
@@ -0,0 +1,12 @@
+"""
+Görevin kullanıcının girdiği iki sayının birbirine tam bölünüp bölünmediğini kullanıcıya bildirmek.
+
+
+Birinci sayıyı giriniz: 6
+İkinci sayıyı giriniz: 3
+6 sayısı 3 sayısına tam bölünüyor.
+
+Birinci sayıyı giriniz: 12
+İkinci sayıyı giriniz: 5
+12 sayısı 5 sayısına tam bölünmüyor.
+"""
