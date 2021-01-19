a485f184929570558f7d7db5069a26be46403447
beratakkaya0 <beratakkaya.2009@gmail.com>
2021-01-19 17:30:26 +0300

    Sekizinci görev tamamlandı

--- a08_gelişmiş_işleçler.py
+++ a08_gelişmiş_işleçler.py
@@ -10,3 +10,12 @@ Birinci sayıyı giriniz: 12
 İkinci sayıyı giriniz: 5
 12 sayısı 5 sayısına tam bölünmüyor.
 """
+from turkish import *
+ilk_sayı=noktalısayı(değeral("Birinci sayıyı giriniz: "))
+ikinci_sayı=noktalısayı(değeral("İkinci sayıyı giriniz: "))
+if ikinci_sayı==0:
+    yazdır("Hiçbir sayı sıfıra bölünemez")
+elif ilk_sayı%ikinci_sayı==0:
+    yazdır(f"{ilk_sayı} sayısı {ikinci_sayı} sayısına tam bölünüyor.")
+else:
+    yazdır(f"{ilk_sayı} sayısı {ikinci_sayı} sayısına tam bölünmüyor.")
