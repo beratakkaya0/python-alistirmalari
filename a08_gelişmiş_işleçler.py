920e6f24990f340c96e0fdb146dce2a92f48fc75
Asocia <akkayas17@itu.edu.tr>
2021-01-21 17:13:10 +0300

    Görevler güncellendi

--- a08_gelişmiş_işleçler.py
+++ a08_gelişmiş_işleçler.py
@@ -30,12 +30,19 @@ Bölüm: 2
 Kalan: 2
 
 """
-birinci_sayı=noktalısayı(değeral("Birinci sayıyı giriniz: "))
-ikinci_sayı=noktalısayı(değeral("İkinci sayıyı giriniz: "))
-if ikinci_sayı==0:
-    yazdır("Hiçbir sayı sıfıra bölünemez")
-else:
-    bölüm=birinci_sayı//ikinci_sayı
-    kalan=birinci_sayı%ikinci_sayı
-    yazdır(f"Bölüm: {bölüm}")
-    yazdır(f"Kalan: {kalan}")
+# birinci_sayı=noktalısayı(değeral("Birinci sayıyı giriniz: "))
+# ikinci_sayı=noktalısayı(değeral("İkinci sayıyı giriniz: "))
+# if ikinci_sayı==0:
+    # yazdır("Hiçbir sayı sıfıra bölünemez")
+# else:
+    # bölüm=birinci_sayı//ikinci_sayı
+    # kalan=birinci_sayı%ikinci_sayı
+    # yazdır(f"Bölüm: {bölüm}")
+    # yazdır(f"Kalan: {kalan}")
+
+"""
+Görevin kullanıcından bir taban ve bir üs alıp taban üzeri üssü hesaplamak.
+Taban sayısını giriniz: 3
+Üs sayısını giriniz: 4
+3^4 = 81
+"""
