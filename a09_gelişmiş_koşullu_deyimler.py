ebce139c3fcc71d39d9699416150da206f0b3ab1
Asocia <akkayas17@itu.edu.tr>
2021-01-30 18:04:14 +0300

    Görevler güncellendi

---
+++ a09_gelişmiş_koşullu_deyimler.py
@@ -0,0 +1,17 @@
+"""
+Görevin girilen ortalamaya göre harf notu yazdıran bir program yazmak.
+
+Girilen ortalamanın harf notu karşılığını aşağıdaki tablodan bulabilirsin.
+0-40   -> F
+41-54  -> D
+55-69  -> C
+70-84  -> B
+85-100 -> A
+
+Notunuz: 73
+B aldınız.
+
+Notunuz: 1000
+Hatalı değer girdiniz.
+"""
+from turkish import *
