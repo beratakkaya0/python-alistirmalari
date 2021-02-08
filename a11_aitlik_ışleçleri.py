8dff3b56583a6d9d8774b6a5a0a7ddea2b29f2a5
Asocia <akkayas17@itu.edu.tr>
2021-02-08 15:44:30 +0300

    Görevler güncellendi

---
+++ a11_aitlik_ışleçleri.py
@@ -0,0 +1,17 @@
+"""
+Görevin girilen bir kelimenin içerisinde kullancının girdiği bir
+harfin olup olmadığını kontrol etmek
+
+Bir kelime giriniz: selam
+Bir harf giriniz: a
+
+Girdiğiniz harf kelimenin içinde vardır.
+
+Bir kelime giriniz: otobüs
+Bir harf giriniz: g
+
+Girdiğiniz harf kelimenin içinde yoktur.
+"""
+from turkish import *
+
+
