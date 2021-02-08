f5d6365f4ab73c70d974b50ac45cbd9d82e8470a
beratakkaya0 <beratakkaya.2009@gmail.com>
2021-02-08 16:18:05 +0300

    On birinci görev tamamlandı.

--- a11_aitlik_ışleçleri.py
+++ a11_aitlik_ışleçleri.py
@@ -13,5 +13,13 @@ Bir harf giriniz: g
 Girdiğiniz harf kelimenin içinde yoktur.
 """
 from turkish import *
-
-
+kelime=değeral("Bir kelime giriniz: ")
+harf=değeral("Bir harf giriniz: ")
+harfin_uzunluğu=uzunluk(harf)
+if harfin_uzunluğu==1:
+    if harf in kelime:
+        yazdır("Girdiğiniz harf kelimenin içinde vardır.")
+    else:
+        yazdır("Girdiğiniz harf kelimenin içinde yoktur.")
+else:
+    yazdır("Girdiğiniz değer hatalıdıdır.")
