61202159aafdbef38f33a1ec861bf539a1e77e5a
Asocia <akkayas17@itu.edu.tr>
2021-02-08 13:37:57 +0300

    Görevler güncellendi

---
+++ a10_değer_atama_işleçleri.py
@@ -0,0 +1,23 @@
+"""
+Görevin girilen değeri bir değişkene atayıp o değişkenin değerini değiştirmek.
+
+Bir sayı giriniz: 5
+Bir kelime yazınız: selam
+
+Girdiğiniz sayının 8 fazlası 13.
+Girdiğiniz kelimenin  büyük harflere çevrilmiş hali: SELAM
+"""
+
+from turkish import *
+sayı = tamsayı(değeral("Bir sayı giriniz: "))
+kelime = değeral("Bir kelime giriniz: ")
+######################################################
+
+
+
+
+######################################################
+yazdır(f"Girdiğiniz sayının 8 fazlası {sayı}.")
+yazdır(f"Girdiğiniz kelimenin  büyük harflere çevrilmiş hali: {kelime}")
+
+
