25ea91dffc4471b109ddc6e6d56e74b8152ccd81
Asocia <akkayas17@itu.edu.tr>
2021-02-10 13:15:38 +0300

    Görevler güncellendi

--- a12_sayaç.py
+++ a12_sayaç.py
@@ -12,7 +12,27 @@ Görevin 1'den 100'e kadar olan sayıları ekrana yazdıran bir program yapmak
 """
 
 from turkish import *
-a=1
-while a<101:
-    yazdır(a)
-    a+=1
+# a=1
+# while a<101:
+    # yazdır(a)
+    # a+=1
+
+
+"""
+Şimdi de kullanıcıdan aldığın alt sınır ve üst sınır arasındaki
+sayıları yazdıran bir program yazman gerekiyor.
+
+Alt sınır: 4
+Üst sınır: 12
+
+4
+5
+6
+.
+.
+.
+11
+12
+
+
+"""
