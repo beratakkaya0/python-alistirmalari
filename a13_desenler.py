467958e5e166e1c75de123168a8b155ba063b3f8
Asocia <akkayas17@itu.edu.tr>
2021-02-11 17:02:21 +0300

    Görevler güncellendi

---
+++ a13_desenler.py
@@ -0,0 +1,13 @@
+"""
+Şimdi işleri biraz zorlaştıralım. İlk aşamada kullanıcıdan aldığın sayıyı kullanarak basit bir
+desen oluşturman gerekiyor. 
+
+Bir sayı giriniz: 5
+
+*
+**
+***
+****
+*****
+"""
+from turkish import *
