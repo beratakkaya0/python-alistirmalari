97870d6a12710bdee7bf12efbb7088ef03254dff
Asocia <akkayas17@itu.edu.tr>
2021-02-11 16:46:24 +0300

    Görevler güncellendi

--- a12_sayaç.py
+++ a12_sayaç.py
@@ -12,6 +12,7 @@ Görevin 1'den 100'e kadar olan sayıları ekrana yazdıran bir program yapmak
 """
 
 from turkish import *
+from time import sleep
 # a=1
 # while a<101:
     # yazdır(a)
@@ -53,12 +54,37 @@ Alt sınır: 5
 10
 12
 """
-alt_sınır=tamsayı(değeral("Alt sınır:"))
-üst_sınır=tamsayı(değeral("Üst sınır:"))
-i=alt_sınır
-while i<=üst_sınır:
-    if i % 2 == 0:
-        yazdır(i)
-    i+=1
+# alt_sınır=tamsayı(değeral("Alt sınır:"))
+# üst_sınır=tamsayı(değeral("Üst sınır:"))
+# i=alt_sınır
+# while i<=üst_sınır:
+    # if i % 2 == 0:
+        # yazdır(i)
+    # i+=1
+
+"""
+Sayaçlar konusundaki son görevin verilen bir değerden geri sayım yapan program yazmak.
+
+Bir sayı giriniz: 10
+
+Geri sayım başladı.
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+0
+Geri sayım bitti!
+
+Küçük bir not:
+Sayıları yazarken arada 1 saniye boşluk olmalı. Örneğin 10 yazdıktan sonra 1 saniye bekleyip
+9 yazmalı. Bunu yapmak için sleep fonksiyonundan yararlanabilirsin. sleep(10) dersen program orada
+10 saniye bekleyip kaldığı yerden devam eder.
+"""
 
 
