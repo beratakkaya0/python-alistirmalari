5a4dc92171094254a817ca3271cb85e05fd0921e
Asocia <akkayas17@itu.edu.tr>
2021-05-02 20:05:08 +0300

    Görevler güncellendi

---
+++ donguler/a14_tahmin.py
@@ -0,0 +1,27 @@
+"""
+Simdiki gorevin basit bir tahmin oyunu hazirlamak. Bilgisayar 1 ile 100 arasinda
+rastgele bir sayi belirleyip senden tahmin yapmani istesin ve sen sayiyi bulana 
+kadar oyunu devam ettirsin:
+
+(B: bilgisayar, O: oyuncu)
+B: Aklimdan bir sayi tuttum; bil bakalim ne tuttum.
+O: 50
+B: yukari
+O: 75
+B: yukari
+O: 87
+B: asagi
+O: 81
+B: asagi
+O: 78
+B: Bildin! Aklimdan tuttugum sayi 78'di.
+
+
+Bilgisayarin urettigi rastgele sayiya 'tuttugum_sayi' isimli degiskenden
+ulasabilirsin.
+"""
+
+from random import randint
+
+tuttugum_sayi = randint(1, 100)
+
