bd5ad6c81e44fed6dde4cb192f9f730baf7888bb
Asocia <akkayas17@itu.edu.tr>
2021-05-03 19:12:54 +0300

    Görevler güncellendi

---
+++ donguler/a16_sayilar.py
@@ -0,0 +1,11 @@
+"""
+Gorevin kullanicidan aldigin sayilarin ortalamasini yazdiran bir program yazmak.
+Kullanici istedigi kadar sayi girebilir. Sayi girisini durdurmak icin 0 girilmelidir.
+
+Sayi girin: 5
+Sayi girin: 10
+Sayi girin: 45
+Sayi girin: 0
+
+3 sayi girdiniz. Girdiginiz sayilarin ortalamasi 20.
+"""
