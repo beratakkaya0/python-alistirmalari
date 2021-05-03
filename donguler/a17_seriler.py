c04ce28a7130060981523ec8a91431f6d296bc03
Asocia <akkayas17@itu.edu.tr>
2021-05-03 21:28:11 +0300

    Görevler güncellendi

---
+++ donguler/a17_seriler.py
@@ -0,0 +1,22 @@
+"""
+Kullanicidan bir sayi al ve 1'den bu sayiya kadar olan tum sayilarin toplamini
+yazdiran bir program yaz.
+
+Bir sayi giriniz: 10
+
+Toplam = 55.
+"""
+
+
+"""
+Simdi de kullanicidan aldigin bir sayiyi kullanarak asagidaki gibi bir seri olustur
+ve toplamini bul.
+
+
+Bir sayi giriniz: 4
+
+2 + 22 + 222 + 2222 = 2468 
+
+"""
+
+
