011f7d0b014244638560e5aa0b98a13213d74c40
Asocia <akkayas17@itu.edu.tr>
2021-05-03 17:52:36 +0300

    Görevler güncellendi

--- donguler/a13_desenler.py
+++ donguler/a13_desenler.py
@@ -3,7 +3,6 @@
 desen oluşturman gerekiyor. 
 
 Bir sayı giriniz: 5
-Ikinci sayıyi giriniz: 5
 
 *
 **
@@ -18,3 +17,19 @@ i=1
 while i<=satır_sayısı :
    print(i*'*')
    i+=1
+
+"""
+Ikinci asamada kullanicidan alacagin sayiya kadar olan sayilarla asagidaki deseni olusturman
+gerekiyor.
+
+Bir sayi giriniz: 9
+1
+22
+333
+4444
+55555
+666666
+7777777
+88888888
+999999999
+"""
