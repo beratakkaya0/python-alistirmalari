c18f4be3504cd59bfda4f112dc4d8e094bb7ec0a
Asocia <akkayas17@itu.edu.tr>
2021-05-05 15:10:07 +0300

    Görevler güncellendi

---
+++ donguler/a18_hesap_makinesi.py
@@ -0,0 +1,35 @@
+"""
+Bu gorevde kullanicinin istedigi kadar islem yapan basit bir hesap makinesi 
+yapman gerekiyor. Asagidaki ornekte sadece 4 islem var ama daha fazla ozellik
+eklemekten cekinme!
+
+(1) topla
+(2) çıkar
+(3) çarp
+(4) böl
+
+Yapmak istediginiz islemin numarasini girin (cikmak icin q): 1
+Toplama islemi icin ilk sayiyi girin: 5
+Toplama islemi icin ikinci sayiyi girin: 10
+5 + 10 = 15
+
+
+(1) topla
+(2) çıkar
+(3) çarp
+(4) böl
+
+Yapmak istediginiz islemin numarasini girin (cikmak icin q): 3
+Carpma islemi icin ilk sayiyi girin: 3
+Carpma islemi icin ikinci sayiyi girin: 7
+3 x 7 = 21
+
+
+(1) topla
+(2) çıkar
+(3) çarp
+(4) böl
+
+Yapmak istediginiz islemin numarasini girin (cikmak icin q): q
+Cikiliyor...
+"""
