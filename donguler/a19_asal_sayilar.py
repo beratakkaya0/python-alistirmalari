e68765d3c186c1b4f337be7085827c3f6fecc7f5
Asocia <akkayas17@itu.edu.tr>
2021-05-06 13:49:59 +0300

    Görevler güncellendi

---
+++ donguler/a19_asal_sayilar.py
@@ -0,0 +1,14 @@
+"""
+1 ve kendisinden baska boleni olmayan sayilara asal sayi denir. 
+Gorevin kullanicidan alinan sayinin asal sayi olup olmadigini soyleyen bir program
+yazmak
+
+Bir sayi giriniz (cikmak icin q): 5
+5 sayisi asaldir.
+
+Bir sayi giriniz (cikmak icin q): 9
+9 sayisi asal degildir.
+
+Bir sayi giriniz (cikmak icin q): q
+Cikiliyor...
+"""
