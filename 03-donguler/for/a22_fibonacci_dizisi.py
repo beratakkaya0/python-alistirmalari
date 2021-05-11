9b2e2705d21c10029434b54b09118345be89ca52
Asocia <akkayas17@itu.edu.tr>
2021-05-11 11:18:36 +0300

    Görevler güncellendi

---
+++ 03-donguler/for/a22_fibonacci_dizisi.py
@@ -0,0 +1,18 @@
+"""
+Gorevin Fibonacci dizisinin ilk N terimini yazdirmak. (N sayisini kullanici belirleyecek)
+Fibonacci dizisi 0, 1 diye baslar ve bundan sonraki her terim kendinden once gelen iki 
+terimin toplami ile bulunur.
+
+Ornek: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ...
+
+N sayisini giriniz: 7
+
+0
+1
+1
+2
+3
+5
+8
+
+"""
