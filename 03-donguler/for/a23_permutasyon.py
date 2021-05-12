d53cac4b85a9dc34c853957cbc9fe6aa8bdec8ce
beratakkaya0 <beratakkaya.2009@gmail.com>
2021-05-12 22:07:31 +0300

    Yirmi ucuncu gorev tamamlandi(bu kadar kolay olacagini tahmin etmemistim).

--- 03-donguler/for/a23_permutasyon.py
+++ 03-donguler/for/a23_permutasyon.py
@@ -22,4 +22,9 @@ Ikinci sayiyi giriniz: 7
 7-6
 7-7
 """
+ilk_sayi=int(input('Ilk sayiyi giriniz: '))
+ikinci_sayi=int(input('Ikinci sayisyi giriniz: ')) 
+for a in range(ilk_sayi,ikinci_sayi+1):
+    for b in range(ilk_sayi,ikinci_sayi+1):
+        print(f'{a}-{b}')
 
