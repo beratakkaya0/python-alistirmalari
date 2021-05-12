f3314070c5dfbb898a48586fe11d985048b1adc1
Asocia <akkayas17@itu.edu.tr>
2021-05-12 20:41:45 +0300

    Görevler güncellendi

---
+++ 03-donguler/for/a23_permutasyon.py
@@ -0,0 +1,25 @@
+"""
+Gorevin kullanicidan iki sayi alarak bu iki sayi arasindaki sayilarin ikili 
+permutasyonlarini yazdiran bir program yazmak
+
+Ilk sayiyi giriniz: 4
+Ikinci sayiyi giriniz: 7
+
+4-4
+4-5
+4-6
+4-7
+5-4
+5-5
+5-6
+5-7
+6-4
+6-5
+6-6
+6-7
+7-4
+7-5
+7-6
+7-7
+"""
+
