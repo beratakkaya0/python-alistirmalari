58dc0f95a388c0dadd7039fcc7109fc29f2e18b7
beratakkaya0 <beratakkaya.2009@gmail.com>
2021-05-13 17:43:04 +0300

    Yirmi besinci gorev tamamlandi.

--- 04-diger-veri-tipleri-ve-metotlar/listeler/a25_listelere_giris.py
+++ 04-diger-veri-tipleri-ve-metotlar/listeler/a25_listelere_giris.py
@@ -6,5 +6,14 @@ Sayilari giriniz: 3, 5, 8, 2, 6
 
 Girdiginiz sayilarin kareleri sirasiyla 9, 25, 64, 4, 36
 """
+sayi_listesi=input('Sayilari giriniz: ').split(', ')
 
+
+sayilarin_karesi=[]
+for sayi in sayi_listesi:
+    sayi=(int(sayi))
+    sayilarin_karesi.append(str(sayi** 2))
+    
+#  print(sayilarin_karesi)
+print('Girdiginiz sayilarin kareleri sirasiyla',', '.join(sayilarin_karesi))
 
