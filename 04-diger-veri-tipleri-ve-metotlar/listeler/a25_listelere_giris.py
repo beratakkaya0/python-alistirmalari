d5207d83d0adf57c948f1f8d2e30b4bee5511543
Asocia <akkayas17@itu.edu.tr>
2021-05-13 15:52:05 +0300

    Görevler güncellendi

---
+++ 04-diger-veri-tipleri-ve-metotlar/listeler/a25_listelere_giris.py
@@ -0,0 +1,10 @@
+"""
+Gorevin kullanicin girdigi sayilari bir listeye atip bu sayilarin karesini yazdiran
+bir program yazmak.
+
+Sayilari giriniz: 3, 5, 8, 2, 6
+
+Girdiginiz sayilarin kareleri sirasiyla 9, 25, 64, 4, 36
+"""
+
+
