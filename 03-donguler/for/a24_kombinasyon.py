3175a0af64ad5149d5681a9417ef6c2a093d1322
Asocia <akkayas17@itu.edu.tr>
2021-05-13 08:05:34 +0300

    Görevler güncellendi

---
+++ 03-donguler/for/a24_kombinasyon.py
@@ -0,0 +1,19 @@
+"""
+Gorevin kullanicidan iki sayi alarak bu iki sayi arasindaki sayilarin ikili 
+kombinasyonlarini yazdiran bir program yazmak
+
+Ilk sayiyi giriniz: 4
+Ikinci sayiyi giriniz: 7
+
+4-4
+4-5
+4-6
+4-7
+5-5
+5-6
+5-7
+6-6
+6-7
+7-7
+"""
+
