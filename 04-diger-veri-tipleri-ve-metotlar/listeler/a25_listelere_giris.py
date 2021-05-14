490deb9bef1d264dac73574424e01c27e2b44026
Asocia <akkayas17@itu.edu.tr>
2021-05-14 12:32:31 +0300

    Görevler güncellendi

--- 04-diger-veri-tipleri-ve-metotlar/listeler/a25_listelere_giris.py
+++ 04-diger-veri-tipleri-ve-metotlar/listeler/a25_listelere_giris.py
@@ -6,14 +6,36 @@ Sayilari giriniz: 3, 5, 8, 2, 6
 
 Girdiginiz sayilarin kareleri sirasiyla 9, 25, 64, 4, 36
 """
-sayi_listesi=input('Sayilari giriniz: ').split(', ')
+# sayi_listesi=input('Sayilari giriniz: ').split(', ')
+#
+#
+# sayilarin_karesi=[]
+# for sayi in sayi_listesi:
+#     sayi=(int(sayi))
+#     sayilarin_karesi.append(str(sayi** 2))
+#     
+# #  print(sayilarin_karesi)
+# print('Girdiginiz sayilarin kareleri sirasiyla',', '.join(sayilarin_karesi))
+#
 
 
-sayilarin_karesi=[]
-for sayi in sayi_listesi:
-    sayi=(int(sayi))
-    sayilarin_karesi.append(str(sayi** 2))
-    
-#  print(sayilarin_karesi)
-print('Girdiginiz sayilarin kareleri sirasiyla',', '.join(sayilarin_karesi))
+"""
+Simdiki gorevin kullanicinin girdigi bir sayinin onceden olusturulmus olan listede
+1'den fazla kez gecip gecmedigini bulmak.
 
+
+Asagidaki tum ornek programlar icin elimizdeki liste soyle olsun: [9, 2, 8, 4, 9, 6]
+Bir sayi giriniz: 8
+8 sayisi listede sadece bir kez geciyor.
+
+Bir sayi giriniz: 9
+9 sayisi listede birden fazla kez geciyor.
+
+Bir sayi giriniz: 3
+3 sayisi listede yok.
+"""
+
+
+from random import randint
+sayi_listesi = [randint(1, 10) for _ in range(15)]
+print(f'{sayi_listesi=}')
