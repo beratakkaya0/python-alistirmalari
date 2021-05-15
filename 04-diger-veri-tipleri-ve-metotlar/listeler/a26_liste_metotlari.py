fb0d1b0c9a80141e29a35ab49a6fcb25530ec336
Asocia <akkayas17@itu.edu.tr>
2021-05-15 12:42:58 +0300

    Görevler güncellendi

---
+++ 04-diger-veri-tipleri-ve-metotlar/listeler/a26_liste_metotlari.py
@@ -0,0 +1,30 @@
+"""
+Gorevin bos bir listeyle baslayip verilen komutlarla listeyi degistiren bir 
+program yazmak. 
+
+>>> print
+[]
+>>> append 1
+>>> append 8
+>>> append 4
+>>> print
+[1, 8, 4]
+>>> insert 0 'selam'
+>>> print
+['selam', 1, 8, 4]
+>>> remove 8
+>>> print
+['selam', 1, 4]
+>>> clear
+>>> print
+[]
+>>> append 3
+>>> append 7
+>>> append 5
+>>> insert 2 3
+>>> print
+[3, 7, 3, 5]
+>>> count 3
+2
+>>> quit
+"""
