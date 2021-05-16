b8bd10e19907f7d1aa573292e4162de3255b57fd
Asocia <akkayas17@itu.edu.tr>
2021-05-16 15:38:12 +0300

    Görevler güncellendi

---
+++ 04-diger-veri-tipleri-ve-metotlar/listeler/a27_xox.py
@@ -0,0 +1,36 @@
+"""
+Gorevin kullaniciya X-O-X oyununu oynatan bir program yazmak.
+Oyunun nasil oynanacagi, kullanicidan girdiyi nasil alacagin sana kalmis.
+
+Tahtayi soyle gosterebilirsin:
+_ _ _
+_ _ _
+_ _ _
+
+Sag ust koseye X konulmus hali:
+_ _ X
+_ _ _
+_ _ _
+
+
+Kullanicinin X ya da O koymak istedigi yeri anlamak icin ilk once satir numarasi
+sonra sutun numarasi alabilirsin:
+3 1 x
+Yani 3. satirdaki ilk sutuna X koy, gibi.
+
+Veya her bir kutucuga numara verip o numara girildiginde o kutucugu doldurabilirsin:
+8 o
+Yani 8. kutucuga O koy, gibi.
+
+X ya da O konulmasi isi sirayla olacagi icin bu isi bilgisayara yaptirabilirsin.
+Boylece kullanicilar x ya da o yazmak zorunda kalmaz. 
+
+
+Bunlar aklima gelen birkac fikir. Oyunu istedigin gibi yazmakta ozgursun.
+
+Kolay gelsin!
+"""
+
+
+
+"""
