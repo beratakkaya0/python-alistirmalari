bcb1427a3c50da8105aeb8c33f561f3f11c62d3e
beratakkaya0 <beratakkaya.2009@gmail.com>
2021-05-18 20:02:45 +0300

    Yirmi yedinci gorev tamamlandi(neden bu kadar zor yaptigini anlamadim.)

--- 04-diger-veri-tipleri-ve-metotlar/listeler/a27_xox.py
+++ 04-diger-veri-tipleri-ve-metotlar/listeler/a27_xox.py
@@ -30,7 +30,47 @@ Bunlar aklima gelen birkac fikir. Oyunu istedigin gibi yazmakta ozgursun.
 
 Kolay gelsin!
 """
-
-
-
-"""
+from random import choice
+kullanici_1=input('Birinci Kullanicinin adini giriniz: ')
+kullanici_2=input('Ikinci kullanicinin adini giriniz: ')
+a=choice([kullanici_1,kullanici_2])
+sayac=2
+print(a.title(),'basliyor.')
+satirlar=[['-','-','-'],['-','-','-'],['-','-','-']]
+while True:
+    b=choice([kullanici_1,kullanici_2])
+    if a!=b:
+        break
+b.title()
+while True:
+    kalan=sayac%2
+    if kalan ==0:
+        simge='X'
+    else:
+        simge='O'
+    if sayac != 2:
+        if kalan ==0:
+            print(f'{a} sende sira!')
+        else:
+            print(f'{b} sende sira!')
+    sayac+=1
+    for satir in satirlar:
+        print(' '.join(satir))
+    satir=input('Satir numarasini giriniz: ')   
+    if satir == 'bitti':
+        print('Oyun bitti!')
+        if kalan == 0:
+            print(f'{b} kazandi!')
+        else:
+            print(f'{a} kazandi!')
+        break
+    elif satir == 'berabere':
+        print('Oyun bitti!')
+        print('Dostluk kazandi!')
+        break
+    sutun=int(input('Sutun numarasini giriniz: '))
+    satir = int(satir)
+    if satir>3 or sutun>3:
+         print('Yanlis bir deger girdiginizden dolayi hakkinizi kaybettiniz.')
+    elif satir <=3:
+        satirlar[satir-1][sutun-1]=simge
