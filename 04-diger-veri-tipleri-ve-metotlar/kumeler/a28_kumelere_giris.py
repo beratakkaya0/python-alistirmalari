bb2a8aebce64f5eb7a3971ab0ae28f46e148ae99
Asocia <akkayas17@itu.edu.tr>
2021-05-20 14:38:36 +0300

    Görevler güncellendi

---
+++ 04-diger-veri-tipleri-ve-metotlar/kumeler/a28_kumelere_giris.py
@@ -0,0 +1,8 @@
+"""
+Gorevin kullanicinin girdigi bir cumle icerisindeki karakterleri bir `kume` icerisinde
+gostermek. Bir `kume`de ayni eleman birden fazla kez bulunamaz. 
+
+Bir cumle giriniz: I'm perfect coder man
+Girdiginiz cumledeki karakterler: {'d', 'm', 'c', 'e', 'p', ' ', 'r', 'o', 'n', 'a', 't', 'f', "'", 'I'}
+"""
+
