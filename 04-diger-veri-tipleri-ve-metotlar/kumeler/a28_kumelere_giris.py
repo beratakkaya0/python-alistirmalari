424932a4070cf3e1020e464c1622882f8dcfb117
beratakkaya0 <beratakkaya.2009@gmail.com>
2021-05-22 14:30:02 +0300

    Yirmi sekizinci gorev tamamlandi.

--- 04-diger-veri-tipleri-ve-metotlar/kumeler/a28_kumelere_giris.py
+++ 04-diger-veri-tipleri-ve-metotlar/kumeler/a28_kumelere_giris.py
@@ -5,4 +5,6 @@ gostermek. Bir `kume`de ayni eleman birden fazla kez bulunamaz.
 Bir cumle giriniz: I'm perfect coder man
 Girdiginiz cumledeki karakterler: {'d', 'm', 'c', 'e', 'p', ' ', 'r', 'o', 'n', 'a', 't', 'f', "'", 'I'}
 """
-
+cumle=input('Bir cumle giriniz: ')
+cumle_kumesi=set(cumle)
+print(cumle_kumesi)
