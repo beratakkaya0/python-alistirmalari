25df1f1cf14c5bc267e5b711dacae5838c0a1b8f
Asocia <akkayas17@itu.edu.tr>
2021-05-23 13:37:12 +0300

    Görevler güncellendi

--- 04-diger-veri-tipleri-ve-metotlar/kumeler/a28_kumelere_giris.py
+++ 04-diger-veri-tipleri-ve-metotlar/kumeler/a28_kumelere_giris.py
@@ -5,6 +5,30 @@ gostermek. Bir `kume`de ayni eleman birden fazla kez bulunamaz.
 Bir cumle giriniz: I'm perfect coder man
 Girdiginiz cumledeki karakterler: {'d', 'm', 'c', 'e', 'p', ' ', 'r', 'o', 'n', 'a', 't', 'f', "'", 'I'}
 """
-cumle=input('Bir cumle giriniz: ')
-cumle_kumesi=set(cumle)
-print(cumle_kumesi)
+# cumle=input('Bir cumle giriniz: ')
+# cumle_kumesi=set(cumle)
+# print(cumle_kumesi)
+
+"""
+Simdi, gorevin ilk asamasinda verdigin ciktiyi alfabetik bir sekilde siralaman gerek.
+Daha sonra her bir harfin kac kez gectigini hesaplayip kullaniciya gostermelisin.
+
+Unutma; kumeler sirali degildir ve ayni eleman birden fazla kez tekrar edemez, 
+listeler siralidir ve ayni eleman birden fazla kez tekrar edebilir.
+
+Bir cumle giriniz: i am perfect coder man
+
+' ': 4
+'a': 2
+'c': 2
+'d': 1
+'e': 3
+'f': 1
+'i': 1
+'m': 2
+'n': 1
+'o': 1
+'p': 1
+'r': 2
+'t': 1
+"""
