42da2938e2fdd744ad21d24e392867174dff92cc
Asocia <akkayas17@itu.edu.tr>
2021-07-01 12:14:58 +0300

    Görevler güncellendi

---
+++ 04-diger-veri-tipleri-ve-metotlar/kumeler/a29_kume_metotlari.py
@@ -0,0 +1,28 @@
+"""
+Gorevin bos bir kumeyle baslayip verilen komutlarla kumeyi degistiren bir 
+program yazmak. 
+
+>>> print
+set()
+>>> add 1
+>>> add 8
+>>> add 4
+>>> print
+{8, 1, 4}
+>>> add 'selam'
+>>> print
+{8, 1, 4, 'selam'}
+>>> remove 8
+>>> print
+{1, 4, 'selam'}
+>>> clear
+>>> print
+set()
+>>> add 3
+>>> add 7
+>>> add 5
+>>> add 3
+>>> print
+{3, 5, 7}
+>>> quit
+"""
